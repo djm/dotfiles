9c2c68cb08d1518456f38cecf62a9d976a3ca697
djm <djm@ezra.local>
2015-02-08 13:19:15 -0500

    Changing layout structure; adding Mac OSX install script for new machines.

---
+++ install.sh
@@ -0,0 +1,21 @@
+git clone git@github.com:djm/dotfiles.git ~/dotfiles/
+
+# Bash config
+ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bash/inputrc ~/.inputrc
+
+# Git config
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
+
+# Mercurial config
+ln -s ~/dotfiles/hg/hgext ~/.hgext
+cp ~/dotfiles/hg/hgrc ~/.hgrc # Copy instead of link due to auth details.
+
+# Vim Config
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/vim/ ~/.vim
+
+# Python Config
+ln -s ~/dotfiles/python/flake8 ~/.config/flake8
---
+++ vim/vim/ftplugin/html.vim
@@ -0,0 +1,2 @@
+jimenezrick/vimerl
+autocmd FileType html :setlocal shiftwidth=2 tabstop=2 softtabstop=2 
